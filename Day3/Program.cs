c6ddb8a1c4cf0a374d475c5b8e32556897cb9144
rik73 <117023539+rik73@users.noreply.github.com>
2024-10-25 18:05:48 +0530

    Add files via upload

---
+++ Day3/Program.cs
@@ -0,0 +1,19 @@
+﻿using Assignments.AssignmentOne;
+using Assignments.AssignmentThree;
+using Assignments.AssignmentTwo;
+
+namespace Assignments
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Assignment1 assign1 = new();
+            //assign1.AssignmentOne();
+            //Assignment2 assignment2 = new();
+            //assignment2.AssignmentTwo();
+            Assignment3 assignment3 = new ();
+            assignment3.AssignmentThree();
+        }
+    }
+}
